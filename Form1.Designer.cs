270c76c5e52a24309c6503b54d8cd2d5e2d90902
Glinsky Ivan <glinsky_ivan@mail.ru>
2021-10-18 11:29:30 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace movePoint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_createPoint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_createPoint
+            // 
+            this.button_createPoint.Location = new System.Drawing.Point(13, 281);
+            this.button_createPoint.Name = "button_createPoint";
+            this.button_createPoint.Size = new System.Drawing.Size(250, 23);
+            this.button_createPoint.TabIndex = 1;
+            this.button_createPoint.Text = "Создать точку";
+            this.button_createPoint.UseVisualStyleBackColor = true;
+            this.button_createPoint.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 323);
+            this.Controls.Add(this.button_createPoint);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_createPoint;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace movePoint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.Red, 2);
+            Brush brush = new SolidBrush(Color.Blue);            
+
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+
+            int sizePoint = 10;
+
+            int posX = 0;
+            int posY = 1;
+
+            int directionX = -1;
+            int directionY = 0;
+
+            while (true)
+            {
+                if (posX >= width - sizePoint || posX <= 0)
+                {
+                    directionX = -directionX;
+                    directionY = rand.Next(1, 4);
+                }
+
+                if ((posY >= height - sizePoint) || (posY <= 0)) directionY = -directionY;
+
+                await Task.Delay(10);
+                graphics.Clear(Color.White);
+                graphics.FillEllipse(brush, posX, posY, sizePoint, sizePoint);
+                posX += directionX;
+                posY += directionY;
+            }
+        }
+    }
+}
